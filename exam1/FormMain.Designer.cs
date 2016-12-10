97abf229571f7680f31d4c89a862f1d1f377448b
DV-13 <gjhneufkmcrbqabkbgg@gmail.com>
2016-12-10 19:51:20 +0300

    Прототип работы с дочерними формами.

---
+++ exam1/FormMain.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace exam1
+{
+	partial class FormMain
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.panel1.Location = new System.Drawing.Point(2, 1);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(980, 50);
+			this.panel1.TabIndex = 1;
+			// 
+			// Form_main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(984, 661);
+			this.Controls.Add(this.panel1);
+			this.IsMdiContainer = true;
+			this.Name = "FormMain";
+			this.Text = "Marathon Skills";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+	}
+}
+
