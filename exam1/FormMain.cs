fed5a41214dd48dc2f9338e55aebb38de885b73a
DV-13 <gjhneufkmcrbqabkbgg@gmail.com>
2016-12-11 20:19:17 +0300

    Запись истории в заголовок для дебага.

--- exam1/FormMain.cs
+++ exam1/FormMain.cs
@@ -12,6 +12,9 @@ namespace exam1
 {
 	public partial class FormMain : Form
 	{
+		public int[] history = new int[10]; //История
+		public int ihistory = 0; //Индекс для истории
+
 		public FormMain()
 		{
 			InitializeComponent();
@@ -21,10 +24,9 @@ namespace exam1
 			MDIChild.MdiParent = this; MDIChild.Show();
 		}
 
-
-
 		private void button_home_Click(object sender, EventArgs e)
 		{
+			Array.Clear(history,0,10); ihistory = 0; //Сброс истории
 			var OldMDIChild = ActiveMdiChild;
 			Form1 MDIChild = new Form1(); //переход с одной формы на другую 
 			MDIChild.MdiParent = this; MDIChild.Show(); OldMDIChild.Close();
@@ -32,7 +34,19 @@ namespace exam1
 
 		private void FormMain_MdiChildActivate(object sender, EventArgs e)
 		{
-			label_timer.Text = ActiveMdiChild.Name.ToString();
+			var newChildName = ActiveMdiChild.Name.ToString();
+			if(newChildName.Length == 5)
+				newChildName = newChildName.Substring(4, 1);
+			else
+				newChildName = newChildName.Substring(4, 2);
+			label_timer.Text = newChildName;
+			history[ihistory] = Int32.Parse(newChildName);
+			ihistory++;
+			label_top.Text = null;
+			foreach (int i in history)
+			{
+				label_top.Text += (i+",");
+			}
 		}
 	}
 }
